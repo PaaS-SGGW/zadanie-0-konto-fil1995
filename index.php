1a0df0e5cd93a095613ccb42031b6afb19ceb86e
fil1995 <fil1995@gmail.com>
2018-11-06 11:23:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-
-echo "hello!<br>";
 $conn = pg_connect(getenv("DATABASE_URL"));
 if($conn) {echo "connected";} else {echo "not connected";}
-
-
+$query = "INSERT INTO users (nick, email, pass) VALUES ('fm','fm@a.pl','akjsdakjdhakdhaskj')";
+$result = pg_query($conn,$query);
+if(!$result){
+    echo "Insert failed:  ";
+    echo pg_result_error($result);
+}
 ?>
